3299fbb9824ca4ff683f7230edb270c2c6462d5a
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-11-15 23:47:07 +0530

    OrderDetailsScreen update

--- src/screens/OrderDetailsScreen/index.js
+++ src/screens/OrderDetailsScreen/index.js
@@ -1,59 +1,50 @@
-import { View, Text, Image, FlatList } from "react-native";
-import orders from "../../../assets/data/orders.json";
-import restaurants from "../../../assets/data/restaurants.json";
+import { View, Text, Image, FlatList, ActivityIndicator } from "react-native";
 import BasketDishItem from "../../components/BasketDishItem";
+
+import orders from "../../../assets/data/orders.json";
+
 import styles from "./styles";
 import { useOrderContext } from "../../contexts/OrderContext";
 import { useEffect, useState } from "react";
 
-const OrderDetailsHeader = () => {
-	const [order] = orders;
-	const {
-		Restaurant: { name, image },
-	} = order;
+const order = orders[0];
 
+const OrderDetailsHeader = ({ order }) => {
 	return (
 		<View>
 			<View style={styles.page}>
-				<Image style={styles.image} source={{ uri: image }} />
-				<View style={styles.row}>
-					<Text style={styles.title}>{name}</Text>
+				<Image source={{ uri: order.Restaurant.image }} style={styles.image} />
+
+				<View style={styles.container}>
+					<Text style={styles.title}>{order.Restaurant.name}</Text>
 					<Text style={styles.subtitle}>{order.status} &#8226; 2 days ago</Text>
+
+					<Text style={styles.menuTitle}>Your orders</Text>
 				</View>
-				<Text style={styles.menu}>Your Order</Text>
 			</View>
 		</View>
 	);
 };
 
-const OrderDetailsScreen = () => {
+const OrderDetails = ({ id }) => {
+	const [order, setOrder] = useState();
+	const { getOrder } = useOrderContext();
+
+	useEffect(() => {
+		getOrder(id).then(setOrder);
+	}, []);
+
+	if (!order) {
+		return <ActivityIndicator size={"large"} color="gray" />;
+	}
+
 	return (
 		<FlatList
-			data={restaurants[0].dishes}
+			ListHeaderComponent={() => <OrderDetailsHeader order={order} />}
+			data={order.dishes}
 			renderItem={({ item }) => <BasketDishItem basketDish={item} />}
-			ListHeaderComponent={OrderDetailsHeader}
 		/>
 	);
 };
 
-// const OrderDetails = ({ id }) => {
-// 	const [order, setOrder] = useState();
-// 	const { getOrder } = useOrderContext();
-
-// 	useEffect(() => {
-// 	  getOrder(id).then(setOrder);
-// 	}, []);
-
-// 	if (!order) {
-// 	  return <ActivityIndicator size={"large"} color="gray" />;
-// 	}
-
-// 	return (
-// 	  <FlatList
-// 		ListHeaderComponent={() => <OrderDetailsHeader order={order} />}
-// 		data={order.dishes}
-// 		renderItem={({ item }) => <BasketDishItem basketDish={item} />}
-// 	  />
-// 	);
-
-export default OrderDetailsScreen;
+export default OrderDetails;
