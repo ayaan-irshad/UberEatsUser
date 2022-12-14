c3d3d83828de43fa47c978a4a0c07931a925b5d1
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-12-14 23:54:49 +0530

    commented styles

--- src/screens/OrderDetailsScreen/styles.js
+++ src/screens/OrderDetailsScreen/styles.js
@@ -1,32 +1,31 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-	page: {
-		flex: 1,
-	},
-	backIcon: {
-		zIndex: 999,
-		position: "absolute",
-		top: 40,
-		left: 10,
-	},
-	image: {
-		width: "100%",
-		aspectRatio: 5 / 3,
-	},
-	title: {
-		fontSize: 35,
-		fontWeight: "300",
-		marginVertical: 10,
-	},
-
-	row: {
-		padding: 15,
-	},
-	menu: {
-		color: "gray",
-		fontSize: 18,
-		paddingLeft: 15,
-		letterSpacing: 0.7,
-	},
+	// page: {
+	// 	flex: 1,
+	// },
+	// backIcon: {
+	// 	zIndex: 999,
+	// 	position: "absolute",
+	// 	top: 40,
+	// 	left: 10,
+	// },
+	// image: {
+	// 	width: "100%",
+	// 	aspectRatio: 5 / 3,
+	// },
+	// title: {
+	// 	fontSize: 35,
+	// 	fontWeight: "300",
+	// 	marginVertical: 10,
+	// },
+	// row: {
+	// 	padding: 15,
+	// },
+	// menu: {
+	// 	color: "gray",
+	// 	fontSize: 18,
+	// 	paddingLeft: 15,
+	// 	letterSpacing: 0.7,
+	// },
 });
