c03e1695734f35a0749f83ddbea1229673a6a9f2
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-12-20 00:00:00 +0530

    comm

--- src/screens/OrderDetailsScreen/index.js
+++ src/screens/OrderDetailsScreen/index.js
@@ -9,22 +9,22 @@ import { useEffect, useState } from "react";
 
 const order = orders[0];
 
-const OrderDetailsHeader = ({ order }) => {
-	return (
-		<View>
-			<View style={styles.page}>
-				<Image source={{ uri: order.Restaurant.image }} style={styles.image} />
+// const OrderDetailsHeader = ({ order }) => {
+// 	return (
+// 		<View>
+// 			<View style={styles.page}>
+// 				<Image source={{ uri: order.Restaurant.image }} style={styles.image} />
 
-				<View style={styles.container}>
-					<Text style={styles.title}>{order.Restaurant.name}</Text>
-					<Text style={styles.subtitle}>{order.status} &#8226; 2 days ago</Text>
+// 				<View style={styles.container}>
+// 					<Text style={styles.title}>{order.Restaurant.name}</Text>
+// 					<Text style={styles.subtitle}>{order.status} &#8226; 2 days ago</Text>
 
-					<Text style={styles.menuTitle}>Your orders</Text>
-				</View>
-			</View>
-		</View>
-	);
-};
+// 					<Text style={styles.menuTitle}>Your orders</Text>
+// 				</View>
+// 			</View>
+// 		</View>
+// 	);
+// };
 
 const OrderDetails = ({ id }) => {
 	const [order, setOrder] = useState();
