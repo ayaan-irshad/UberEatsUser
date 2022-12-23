215b0b95f6cf55328182b840ebe43bd324c1f355
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-12-23 23:56:24 +0530

    image update

--- src/screens/OrderDetailsScreen/index.js
+++ src/screens/OrderDetailsScreen/index.js
@@ -13,7 +13,7 @@ const OrderDetailsHeader = ({ order }) => {
 	return (
 		<View>
 			<View style={styles.page}>
-				{/* <Image source={{ uri: order.Restaurant.image }} style={styles.image} /> */}
+				<Image source={{ uri: order.Restaurant.image }} style={styles.image} />
 
 				<View style={styles.container}>
 					<Text style={styles.title}>{order.Restaurant.name}</Text>
