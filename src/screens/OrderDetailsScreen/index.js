aca930ab69afdf48aec80e625ab7a7182ac8a520
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-12-25 23:57:28 +0530

    newline

--- src/screens/OrderDetailsScreen/index.js
+++ src/screens/OrderDetailsScreen/index.js
@@ -14,7 +14,7 @@ const OrderDetailsHeader = ({ order }) => {
 		<View>
 			<View style={styles.page}>
 				<Image source={{ uri: order.Restaurant.image }} style={styles.image} />
-
+				{/* // bew line char addd */}
 				<View style={styles.container}>
 					<Text style={styles.title}>{order.Restaurant.name}</Text>
 					<Text style={styles.subtitle}>{order.status} &#8226; 2 days ago</Text>
