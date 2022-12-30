39f62975ad5fc621c64e3723d9543cf474eeccfd
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-12-30 23:57:43 +0530

    cleared space

--- src/screens/OrderDetailsScreen/index.js
+++ src/screens/OrderDetailsScreen/index.js
@@ -17,7 +17,6 @@ const OrderDetailsHeader = ({ order }) => {
 				<View style={styles.container}>
 					<Text style={styles.title}>{order.Restaurant.name}</Text>
 					<Text style={styles.subtitle}>{order.status} &#8226; 2 days ago</Text>
-
 					<Text style={styles.menuTitle}>Your orders</Text>
 				</View>
 			</View>
