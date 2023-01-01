cec3c31edfdbac75a27936f8fee94e2d670167a3
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2023-01-01 23:07:02 +0530

    removed styles

--- src/screens/BasketScreen/index.js
+++ src/screens/BasketScreen/index.js
@@ -43,50 +43,45 @@ const BasketScreen = () => {
 };
 
 const styles = StyleSheet.create({
-	page: { flex: 1, width: "100%", paddingVertical: 30, padding: 10 },
-
-	name: {
-		fontSize: 30,
-		fontWeight: "600",
-		marginVertical: 10,
-	},
-	description: {
-		color: "gray",
-		fontWeight: "bold",
-		fontSize: 19,
-	},
-	separator: {
-		height: 1,
-		backgroundColor: "lightgrey",
-		marginVertical: 10,
-	},
-
-	row: {
-		alignItems: "center",
-		flexDirection: "row",
-		marginVertical: 15,
-	},
-
-	quantityContainer: {
-		backgroundColor: "lightgray",
-		paddingHorizontal: 5,
-		paddingVertical: 2,
-		borderRadius: 3,
-		marginRight: 10,
-	},
-
-	button: {
-		backgroundColor: "black",
-		marginTop: "auto",
-		padding: 20,
-		alignItems: "center",
-	},
-
-	buttonText: {
-		color: "white",
-		fontWeight: "600",
-		fontSize: 20,
-	},
+	// page: { flex: 1, width: "100%", paddingVertical: 30, padding: 10 },
+	// name: {
+	// 	fontSize: 30,
+	// 	fontWeight: "600",
+	// 	marginVertical: 10,
+	// },
+	// description: {
+	// 	color: "gray",
+	// 	fontWeight: "bold",
+	// 	fontSize: 19,
+	// },
+	// separator: {
+	// 	height: 1,
+	// 	backgroundColor: "lightgrey",
+	// 	marginVertical: 10,
+	// },
+	// row: {
+	// 	alignItems: "center",
+	// 	flexDirection: "row",
+	// 	marginVertical: 15,
+	// },
+	// quantityContainer: {
+	// 	backgroundColor: "lightgray",
+	// 	paddingHorizontal: 5,
+	// 	paddingVertical: 2,
+	// 	borderRadius: 3,
+	// 	marginRight: 10,
+	// },
+	// button: {
+	// 	backgroundColor: "black",
+	// 	marginTop: "auto",
+	// 	padding: 20,
+	// 	alignItems: "center",
+	// },
+	// buttonText: {
+	// 	color: "white",
+	// 	fontWeight: "600",
+	// 	fontSize: 20,
+	// },
 });
 
 export default BasketScreen;
